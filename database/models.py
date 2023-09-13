86138125dc689c6e2f12e91faa83252d905eaa56
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-09-13 10:08:13 +0000

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String
 
 from database.database import Base
 
@@ -6,4 +6,5 @@ from database.database import Base
 class Person(Base):
     __tablename__ = "persons"
 
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
